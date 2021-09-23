acc8a717249edc5a25cc7e984e39c94e44697cf2
svallabaneni <vallabaneni.sandeep@orbcomm.com>
2021-09-23 11:55:18 +0530

    put delete added for Departments service

--- src/main/java/com/gi/portal/controller/DepartmentController.java
+++ src/main/java/com/gi/portal/controller/DepartmentController.java
@@ -3,6 +3,8 @@ package com.gi.portal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,5 +36,20 @@ public class DepartmentController {
     public Department findByDepartmentId(@PathVariable("id") Long departmentId) {
         return departmentService.findByDepartmentId(departmentId);
     }
-
+	
+	@PutMapping("/")
+	public Department updateDepartment(@RequestBody Department department) {
+		return departmentService.saveDepartment(department);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Long> deleteDepartment(@PathVariable("id") Long departmentId) {
+		Department employee = departmentService.findByDepartmentId(departmentId);
+		if(employee != null) {
+			departmentService.deleteByDepartmentId(departmentId);
+		}else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+        return new ResponseEntity<>(departmentId, HttpStatus.OK);
+    }
 }
