5ec2949f4d23b0bfaa1719d1fc9d5bc270e87bb9
svallabaneni <vallabaneni.sandeep@orbcomm.com>
2021-09-23 19:50:22 +0530

    added Employee service, recreation of database on server start removed

--- src/main/java/com/gi/portal/controller/EmployeeController.java
+++ src/main/java/com/gi/portal/controller/EmployeeController.java
@@ -3,9 +3,13 @@ package com.gi.portal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gi.portal.entity.Employee;
 import com.gi.portal.service.EmployeeService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/employees")
+@Slf4j
 public class EmployeeController {
 	
 	@Autowired
@@ -33,6 +40,22 @@ public class EmployeeController {
 	@GetMapping("/{id}")
     public Employee findByEmployeeId(@PathVariable("id") Long employeeId) {
         return employeeService.findByEmployeeId(employeeId);
+    }
+	
+	@PutMapping("/")
+	public Employee updateDepartment(@RequestBody Employee employee) {
+		return employeeService.saveEmployee(employee);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Long> deleteDepartment(@PathVariable("id") Long employeeId) {
+		Employee employee = employeeService.findByEmployeeId(employeeId);
+		if(employee != null) {
+			employeeService.deleteByEmployeeId(employeeId);
+		}else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+        return new ResponseEntity<>(employeeId, HttpStatus.OK);
     }
 
 }
